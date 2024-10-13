c394c78b0204adb47c3ffb0546caaad96a914bdd
Brandon Bullock <300088062@fvtc.edu>
2024-10-12 23:47:10 -0500

    Added Carousel assignment. Still need to finish getting it working.

--- site.js
+++ site.js
@@ -22,3 +22,56 @@ localStorage.setItem("It's a secret to everybody.", secretMessage)
     else{
         Welcome.textContent = 'Good Evening to you Friends'
     }
+
+    /*10/12/2024 Carousel*/
+    const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    ].map(url => { (new Image()).src = url; return url })
+
+    const images = document.querySelectorAll('#carousel img')
+
+    let currentImage = 0
+    const showImages = () => {
+    const offset = currentImage % urls.length
+
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+    }
+
+    showImages()   
+    
+    setTimeout(() => {
+        // code to run ONCE after 5 seconds
+    }, 5000)
+    
+    setInterval(() => {
+        // code to run EVERY 5 seconds
+    }, 5000)
+
+    function previousImage()
+    {
+            currentImage = currentImage--;
+            showImages();
+    }
+    const prevButton = document.getElementById("#prev");
+    prevButton.addEventListener("Click", () => 
+    {
+        previousImage();
+    })
+    
+    function nextImage()
+    {
+            currentImage = currentImage++;
+            showImages;
+    }
+    const nextButton = document.getElementById("#next");
+    nextButton.addEventListener("Click", () => 
+    {
+        nextImage();
+    })
