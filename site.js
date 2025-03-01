69e6ae1e7b9994e79bedc2fbc81d608101e3ad66
Brandon Bullock <300088062@fvtc.edu>
2025-02-28 20:00:55 -0600

    Added carousel

--- site.js
+++ site.js
@@ -19,3 +19,57 @@ const Welcome = document.getElementById('Welcome')
     else{
         Welcome.textContent = 'Evening seekers of this page~'
     }
+
+    /*Carousel 02/28/2025*/
+        const urls = [
+            'http://www.dorkistan.com/Images/PFRPG_Tengu.gif', //Tengu
+            'https://files.d20.io/images/366922689/Mxo_koiV5H6PTU5lLsIZbA/original.png', //Human
+            'https://files.d20.io/images/86890186/oa6nxXljyf2iqN4LzGooYA/med.png?1563574007', //Dwarf
+            'https://theonyxpath.com/wp-content/uploads/2016/03/Scarred-One_BryanSyme.png', //Yoski
+            'https://2e.aonprd.com/Images/Ancestries/Elf02.png' //Elf
+            
+        ].map(url => { (new Image()).src = url; return url })
+        
+        const images = document.querySelectorAll('#carousel img')
+        
+        let currentImage = 0
+        const showImages = () => {
+            const offset = currentImage % urls.length
+            images.forEach((image, index) => {
+                const imageIndex = (index + offset + urls.length) % urls.length
+                image.src = urls[imageIndex]
+            })
+        }
+        
+        showImages()
+
+        const nextButton = document.querySelector('#next');
+        const prevButton = document.querySelector('#prev');        
+
+        const nextImage = () => {
+            currentImage++;
+            showImages()
+        }
+        const prevImage= () => {
+            currentImage--;
+            showImages()
+        }
+
+        setTimeout(() => {
+            // code to run ONCE after 5 seconds
+            nextImage()
+        }, 5000)
+        setInterval(() => {
+            // code to run EVERY 5 seconds
+            setInterval()
+        }, 5000)
+
+        
+        prevButton.addEventListener('click', () => {
+            prevImage()
+        })        
+        nextButton.addEventListener('click', () => {
+            nextImage()
+        })
+
+    /*Carousel 02/28/2025*/
