78a7852d00e9fd6fa810a7f8c961cce1f5f08f74
Brandon Bullock <300088062@fvtc.edu>
2026-02-07 20:13:42 -0600

    added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,19 @@
 console.log("Console.log: 'Testing'. This works~ ")
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+let welcomeMessage;
+
+if (isMorning){
+    welcomeMessage = 'Good Morning'
+}else if (isAfternoon){
+    welcomeMessage = 'Good Afternoon'
+}else{
+    welcomeMessage = 'Good Evening'
+}
+
+document.querySelector('#welcome').textContent = welcomeMessage
