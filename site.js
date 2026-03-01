5facf3a926d7059bc8844cd1ebc51dd000187759
Brandon Bullock <300088062@fvtc.edu>
2026-02-28 20:38:14 -0600

    Added Carousel functionallity and changed carousel images for fun.

--- site.js
+++ site.js
@@ -21,3 +21,50 @@ document.querySelector('#welcome').textContent = welcomeMessage
 
 const secretMessage = "It's a secret to everybody. The legend of Zelda: Staring Link (not Zelda..)."
 localStorage.setItem('Secret Message', secretMessage)
+
+// Assignment 5 - Carousel
+const urls = [
+    // looked for something slightly diffrent from the provided images. Hope you like them.
+    //'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://2e.aonsrd.com/images/classes/classes2_mystic_iconic_chk_chk.webp',
+    //'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://2e.aonsrd.com/images/classes/classes6_witchwarper_iconic_zemir.webp',
+    //'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://2e.aonsrd.com/images/classes/classes4_solarian_iconic_dae.webp',
+    //'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://2e.aonsrd.com/images/classes/classes1_envoy_iconic_navasi.webp',
+    //'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    'https://2e.aonsrd.com/images/classes/classes5_soldier_iconic_obozaya.webp',
+    'https://2e.aonsrd.com/images/classes/classes3_operative_iconic_iseph.webp'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+
+prevButton.addEventListener('click', () => {    
+    currentImage = (currentImage - 1 + urls.length) % urls.length
+    showImages()
+})
+
+nextButton.addEventListener('click', () => {
+    currentImage = (currentImage + 1) % urls.length
+    showImages()
+})
+
+const interval = setInterval(() => {
+    currentImage = (currentImage + 1) % urls.length
+    showImages()
+}, 5000)
+
+showImages()
